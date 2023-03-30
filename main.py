7659c3ce53f97b40c7f43faa2ec939e6922fa124
KairatAmanaliev <126100805+KairatAmanaliev@users.noreply.github.com>
2023-03-30 18:14:22 +0600

    hw4

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from aiogram.utils import executor
 import logging
 
-from config import dp
+from config import dp, bot, ADMIN
 from handlers import client, callback, admin, extra, fsmAdminMentor
+from database.bot_db import sql_create
+
+
+async def on_startup(_):
+    await bot.send_message(ADMIN[0], 'Я родился!')
+    sql_create()
+
+async def on_shutdown(dp):
+    await bot.send_message(ADMIN[0], 'Я спать')
+
 
 client.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
@@ -13,4 +23,4 @@ extra.register_handlers_client(dp)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup, on_shutdown=on_shutdown)
